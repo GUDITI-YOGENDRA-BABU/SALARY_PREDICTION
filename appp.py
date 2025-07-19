64922d65576a221a72520e7ac938cd25ac2dcd6b
GUDITI-YOGENDRA-BABU <guditiyogendrababu@gmail.com>
2025-07-19 19:31:01 +0530

    Update appp.py

--- appp.py
+++ appp.py
@@ -13,6 +13,8 @@ le_comp = joblib.load("le_comp.pkl")
 le_dept = joblib.load("le_dept.pkl")
 le_skill = joblib.load("le_skill.pkl")
 le_edu = joblib.load("le_edu.pkl")
+le_sal = joblib.load("le_sal.pkl")
+
 
 # Load feature names and encoders
 feature_names = joblib.load("feature_names.pkl")
@@ -26,12 +28,12 @@ occupation = st.sidebar.selectbox("Job Title", [
 ])
 
 # Fixed example inputs (or you can make these also user selectable)
-experience_level = "Mid"
-employment_type = "Full-time"
-location = "Chicago"
-company_size = "Medium"
-department = "R&D"
-primary_skill = "Python"
+experience_level = st.sidebar.selectbox("experience_level", ["Entry","Mid","Senior"])
+employment_type = st.sidebar.selectbox("employment_type", ["Contract","Full-time","Part-time"])
+location = st.sidebar.selectbox("location", ["Chicago","New York","Chicago","Remote","Los Angeles","Seattle","Boston","Austin","San Jose","San Francisco"])
+company_size = st.sidebar.selectbox("company_size", ["Large","Medium","Small"])
+department = st.sidebar.selectbox("department", ["Operations","R&D","HR","IT","Analytics","Product",])
+primary_skill = st.sidebar.selectbox("", ['Project Management","Machine Learning","Cloud","Python","Java","DevOps","C++","Deep Learning","Data Visualization","SQL"])
 
 # 1. Display human-readable inputs
 input_display_df = pd.DataFrame([{
@@ -62,10 +64,15 @@ input_encoded_df = pd.DataFrame([{
 input_encoded_df = input_encoded_df[feature_names]
 
 # Show readable inputs to user
-st.write("### 🔍 Input Features (Readable)")
+st.write("Input Features (Readable)")
 st.table(input_display_df)
 
 # Predict button
 if st.button("Predict Salary Class"):
     prediction = model.predict(input_encoded_df)
-    st.success(f"✅ Predicted Salary Class: {prediction[0]}")
+    decoded_salary = le_sal.inverse_transform(prediction)[0]
+    st.success(f"Predicted Salary : {decoded_salary}")
+
+
+   
+
