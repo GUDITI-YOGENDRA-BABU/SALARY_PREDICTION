3c4e1e6c0af547ead90edd4e79f9fcf56f157ac1
GUDITI-YOGENDRA-BABU <guditiyogendrababu@gmail.com>
2025-07-19 19:36:05 +0530

    Update appp.py

--- appp.py
+++ appp.py
@@ -18,16 +18,12 @@ le_sal = joblib.load("le_sal.pkl")
 
 # Load feature names and encoders
 feature_names = joblib.load("feature_names.pkl")
-
-# User inputs
 age = st.sidebar.slider("Age", 18, 65, 30)
 education = st.sidebar.selectbox("Education Level", ["Bachelor's", "Master's", "PhD", "Diploma"])
 occupation = st.sidebar.selectbox("Job Title", [
     "AI Researcher", "ML Engineer", "Software Engineer", "System Admin", "Data Engineer",
     "Data Scientist", "Web Developer", "Product Manager", "Data Analyst", "DevOps Engineer"
 ])
-
-# Fixed example inputs (or you can make these also user selectable)
 experience_level = st.sidebar.selectbox("experience_level", ["Entry","Mid","Senior"])
 employment_type = st.sidebar.selectbox("employment_type", ["Contract","Full-time","Part-time"])
 location = st.sidebar.selectbox("location", ["Chicago","New York","Chicago","Remote","Los Angeles","Seattle","Boston","Austin","San Jose","San Francisco"])
@@ -35,7 +31,6 @@ company_size = st.sidebar.selectbox("company_size", ["Large","Medium","Small"])
 department = st.sidebar.selectbox("department", ["Operations","R&D","HR","IT","Analytics","Product",])
 primary_skill = st.sidebar.selectbox("primary_skill", ["Project Management","Machine Learning","Cloud","Python","Java","DevOps","C++","Deep Learning","Data Visualization","SQL"])
 
-# 1. Display human-readable inputs
 input_display_df = pd.DataFrame([{
     'age': age,
     'education_level': education,
@@ -48,7 +43,7 @@ input_display_df = pd.DataFrame([{
     'primary_skill': primary_skill
 }])
 
-# 2. Encoded version for prediction
+
 input_encoded_df = pd.DataFrame([{
     'age': age,
     'education_level': le_edu.transform([education])[0],
@@ -63,8 +58,8 @@ input_encoded_df = pd.DataFrame([{
 
 input_encoded_df = input_encoded_df[feature_names]
 
-# Show readable inputs to user
-st.write("Input Features (Readable)")
+
+st.write("Input Features ")
 st.table(input_display_df)
 
 # Predict button
